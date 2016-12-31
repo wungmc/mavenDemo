3a0460e1874a21d70af615e9347b88ee44c50ec4
wungmc <wungcc@gmail.com>
2017-01-01 03:14:38 +0800

    Initial commit

---
+++ src/main/java/com/wung/maven/Greeter.java
@@ -0,0 +1,11 @@
+package com.wung.maven;
+
+/**
+ *
+ * Created by wung on 2017/1/1.
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/main/java/com/wung/maven/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.wung.maven;
+
+/**
+ *
+ * Created by wung on 2017/1/1.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
