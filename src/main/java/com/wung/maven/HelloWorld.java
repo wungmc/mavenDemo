d237fd1c6402752b50339b5d65daa52aa444b9aa
wungmc <wungcc@gmail.com>
2017-01-02 00:15:04 +0800

    add dependencies

--- src/main/java/com/wung/maven/HelloWorld.java
+++ src/main/java/com/wung/maven/HelloWorld.java
@@ -1,11 +1,15 @@
 package com.wung.maven;
 
+import org.joda.time.LocalTime;
+
 /**
  *
  * Created by wung on 2017/1/1.
  */
 public class HelloWorld {
     public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println(currentTime);
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
